11b3e57333589407a7daf2c97f8dd01b66e7f56f
Mostafa Mohamad <mostafamohm2002ead@gmail.com>
2025-02-11 22:20:33 +0300

    Add villa controller and their views

---
+++ VillaWeb/Controllers/VillaController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _Db;
+        public VillaController(ApplicationDbContext Db)
+        {
+            _Db = Db;
+        }
+        public IActionResult Index()
+        {
+            var Villas = _Db.Villas.ToList();
+            return View(Villas);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if(obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("Description", "Name and Description cannot be the same");
+            }
+            if (ModelState.IsValid)
+            {
+                _Db.Villas.Add(obj);
+                _Db.SaveChanges();
+                return RedirectToAction("index");
+            }
+            return View(obj);
+        }
+        public IActionResult Update(int id)
+        {
+            Villa? ObjFromDb = _Db.Villas.FirstOrDefault(v => v.Id == id);
+            if(ObjFromDb == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(ObjFromDb);
+        }
+    }
+}
