65cb70eafae824b30806c00308885763c79d91cd
Mostafa Mohmead <122389002+mu17m@users.noreply.github.com>
2025-09-21 15:17:43 +0300

    Delete VillaWeb directory

--- VillaWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WhiteLagoonWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
